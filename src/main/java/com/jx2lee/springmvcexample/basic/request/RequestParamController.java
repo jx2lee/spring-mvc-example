f9d276d021b35a82f5a913b0da057ece2df7193e
jx2lee <jaejun.lee.1991@gmail.com>
2021-08-09 16:23:53 +0900

    섹션 6. 스프링 MVC - 기본 기능: HTTP 요청 파라미터: @RequestParam
    * @RequestParam : 파라미터 이름으로 바인딩
    * @ResponseBody : View 조회를 무시하고, HTTP message body에 직접 해당 내용 입력
      * 애노테이션을 완전히 생략해도 되는데, 너무 없는 것도 약간 과하다는 주관적 생각
    
    * @RequestParamRequired
      * /request-param?username=
      * 파라미터 이름만 있고 값이 없는 경우 빈문자로 통과
    * 주의! - 기본형(primitive)에 null 입력
      * @RequestParam(required = false) int age
      * null 을 int 에 입력하는 것은 불가능(500 예외 발생)
      * 따라서 null 을 받을 수 있는 Integer 로 변경하거나, defaultValue 사용
    
    * DefaultValue
      * 파라미터에 값이 없는 경우 defaultValue 를 사용하면 기본 값을 적용할 수 있다.
      * 이미 기본 값이 있기 때문에 required 는 의미가 없다.
    
    * @RequestParam Map<String, Object>
      * 파라미터의 값이 1개가 확실하다면 Map 을 사용해도 되지만, 그렇지 않다면 MultiValueMap 을 사용

--- src/main/java/com/jx2lee/springmvcexample/basic/request/RequestParamController.java
+++ src/main/java/com/jx2lee/springmvcexample/basic/request/RequestParamController.java
@@ -3,10 +3,13 @@ package com.jx2lee.springmvcexample.basic.request;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -19,5 +22,60 @@ public class RequestParamController {
         log.info("username={}, age={}", username, age);
 
         response.getWriter().write("ok");
+    }
+
+    @ResponseBody // @RestController 로 변환해도 된다.
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge) {
+
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age) {
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    // 과도한 생략은 좋지 않다. (영한쌤 의견)
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age) {
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") Integer age) {
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
     }
 }
